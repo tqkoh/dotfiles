be355a2cd95528be06046c4b9bd5fbd261d5e681
tqkoh <shktmygs@gmail.com>
2023-04-07 00:16:45 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -2,21 +2,25 @@
 # backup
 if [ -f ~/.bashrc ]; then
     cp --backup=numbered ~/.bashrc ~/.bashrc.bak
+    rm ~/.bashrc
 elif [ -L ~/.bashrc ]; then
     rm ~/.bashrc
 fi
 if [ -f ~/.vimrc ]; then
     cp --backup=numbered ~/.vimrc ~/.vimrc.bak
+    rm ~/.vimrc
 elif [ -L ~/.vimrc ]; then
     rm ~/.vimrc
 fi
 if [ -f ~/.tmux.conf ]; then
     cp --backup=numbered ~/.tmux.conf ~/.tmux.conf.bak
+    rm ~/.tmux.conf
 elif [ -L ~/.tmux.conf ]; then
     rm ~/.tmux.conf
 fi
 if [ -d ~/.tmux ]; then
     cp -r --backup=numbered ~/.tmux ~/.tmux.bak
+    rm -r ~/.tmux
 elif [ -L ~/.tmux ]; then
     rm ~/.tmux
 fi
